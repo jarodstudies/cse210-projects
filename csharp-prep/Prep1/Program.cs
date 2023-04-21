6a11b1288cfea57fc52005e30c7580ac723e925c
jarodstudies <jarodosorioramos@gmail.com>
2023-04-21 13:07:57 +0200

    Assignmnet 1 of Week 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
